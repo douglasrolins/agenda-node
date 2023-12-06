1ed762d35b8585046a0a1935ef36913d48c03634
douglasrolins <douglasrolins@gmail.com>
2023-12-06 15:53:39 -0300

    Configuração com BD
    Envio do arquivo de configuração com o banco de dados

---
+++ config/dbconfig.js
@@ -0,0 +1,9 @@
+// config/dbconfig.js
+const dbConfig = {
+    host: '',
+    user: '', // Seu usuário do MySQL
+    password: '', // Sua senha do MySQL
+    database: '', // Nome do seu banco de dados
+  };
+  
+  module.exports = dbConfig;
