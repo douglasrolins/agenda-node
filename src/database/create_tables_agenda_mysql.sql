d33850dabce98ad188b75e4a0686297f9dfdb213
douglasrolins <douglasrolins@gmail.com>
2023-12-07 20:29:23 -0300

    Scripts SQL para criar as tabelas]

---
+++ src/database/create_tables_agenda_mysql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE companies (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40),
+    address VARCHAR(200),
+    PRIMARY KEY (id)
+);
---
+++ src/database/create_tables_agenda_postgres.sql
@@ -0,0 +1,5 @@
+CREATE TABLE companies (
+    id bigserial PRIMARY KEY,
+    name varchar(40),
+    address varchar(200)
+);
