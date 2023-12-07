b2561965c71bab39b07d93f968a4409ed8b4c865
douglasrolins <douglasrolins@gmail.com>
2023-12-07 14:28:48 -0300

    Disponiblização do arquivo de exemplo para configuração com banco de dados

---
+++ config/dbconfig-sample.js
@@ -0,0 +1,9 @@
+// Renomear o arquivo para: config/dbconfig.js
+const dbConfig = {
+    host: '', // Servidor do banco de dados
+    user: '', // Nome do usuário do MySQL
+    password: '', // Senha do MySQL
+    database: '', // Nome do banco de dados
+  };
+  
+  module.exports = dbConfig;
